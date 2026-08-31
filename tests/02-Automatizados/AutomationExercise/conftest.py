2257150a77236c1949d551357b8d0c518e0ec8a6
Salva_BsK <serkover@gmail.com>
2026-08-31 14:05:40 +0200

    automationexercise-yml-added3

--- tests/02-Automatizados/AutomationExercise/conftest.py
+++ tests/02-Automatizados/AutomationExercise/conftest.py
@@ -1,6 +1,11 @@
 
-import pytest
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent))
+
 import constants
+import pytest
 import allure
 import requests
 from playwright.sync_api import expect
